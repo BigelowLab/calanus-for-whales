b1322605dcd452185831da7e8aff58bcb78f9d96
Camille <chross@icloud.com>
2020-10-19 14:18:00 -0400

    Converted to function

--- Code/write_config.R
+++ Code/write_config.R
@@ -5,39 +5,36 @@
 # ---- Load libraries ----
 library(yaml)
 
-# ---- Initialize parameters ----
+# ---- Function to write configuration file ----
+#'@param version <chr> model version in "vx.x.x" format
+#'@param species <chr> species for which the model is run; one of "cfin", "ctyp", or "pcal"
+#'@param env_covars <list> list of environmental covaraites; options are wind, fetch, sst, sss,
+# bott, bots, bat, slope, dist, chl, lag_sst, int_chl
+#'@param anomaly <logical> whether or not model converts data to anomalies
+#'@param threshold <num> right whale feeding thershold for biomod models
+#'@param years <vector> years to run model for between 2000 and 2017
+#'@param fp_md <filepath> filepath to model data
+#'@param fp_covars <filepath> filepath to environmental covariates
+#'@param fp_out <filepath> filepath to store model output
+#'@param datasets <chr> datasets to use for mgcv gams and gbm brts
+#'@param biomod_dataset <chr> datasets to use for biomod models
+#'@param format_data <logical> whether or not to format sightings and covar data
+#'@param fp_zpd <filepath> filepath to zooplankton data if format_data is TRUE
+write_config <- function(version, species, env_covars, anomaly,
+                         threshold, years, fp_md, fp_covars,
+                         fp_out, datasets, biomod_dataset, format_data, 
+                         fp_zpd) {
 
-version <- "v0.2.7"
-species <- c("cfin", "ctyp", "pseudo")[3]
-env_covars <- c("sst", "chl", "bat", "sss", "wind")
-anomaly <- FALSE
-threshold <- 10000
-years <- 2000:2017
+  # ---- Initialize yaml parameters ----
+  x <- list(version = version, species = species, env_covars = env_covars, anomaly = anomaly, threshold = threshold, years = years, 
+           fp_md = fp_md, datasets = datasets, biomod_dataset = biomod_dataset, fp_covars = fp_covars,
+           fp_out = fp_out, format_data = format_data, fp_zpd = fp_zpd)
+  
+  # ---- Create directory ----
+  dir.create(file.path("./calanus-for-whales/Versions/", version))
+  
+  # ---- Write yaml ----
+  write_yaml(x, file.path("./calanus-for-whales/Versions", version, paste0(version, ".yaml")))
 
-# ---- Initialize relative filepaths ----
-fp_md <- "./calanus_data/Data/Databases/zooplankton_covar_data"
-fp_covars <- "./Env_Covars"
-# Must be full filepath to use rmarkdown::render(); see line 51
-fp_out <- "~/Desktop/Calanus_Project/projects/calanus4whales/Models"
-
-# ---- Initialize dataset ----
-datasets <- c("CPR", "NOAA_CPR")
-biomod_dataset <- "ECOMON"
-
-# ---- Initialize data formatting options ----
-format_data <- FALSE
-fp_zpd <- NULL
-
-# ---- Initialize yaml parameters ----
-x <- list(version = version, species = species, env_covars = env_covars, anomaly = anomaly, threshold = threshold, years = years, 
-         fp_md = fp_md, datasets = datasets, biomod_dataset = biomod_dataset, fp_covars = fp_covars,
-         fp_out = fp_out, format_data = format_data, fp_zpd = fp_zpd)
-
-# ---- Create directory ----
-dir.create(file.path("./calanus-for-whales/Versions/", version))
-
-# ---- Write yaml ----
-write_yaml(x, file.path("./calanus-for-whales/Versions", version, paste0(version, ".yaml")))
-
-
+}
 
